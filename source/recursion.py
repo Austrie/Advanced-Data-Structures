d0f74a0e824959f8b9c2f245952e96a87976d100
Shane Austrie <yoyo.rom.dev@gmail.com>
2019-05-02 10:34:22 -0700

    Committing changes from May 23, 2018. Proof: https://drive.google.com/file/d/1aO8PUDq2NjI0YMfD6r4f6e9lys-Bq2W8/view?usp=sharing

--- source/recursion.py
+++ source/recursion.py
@@ -1,4 +1,5 @@
 #!python
+import time
 
 def fibonacci(n):
     """fibonacci(n) returns the n-th number in the Fibonacci sequence,
@@ -7,7 +8,7 @@ def fibonacci(n):
     fibonacci(1) = 1
     fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2), for n > 1"""
     # Check if n is negative or not an integer (invalid input)
-    if n < 0 or not isinstance(n, int):
+    if not isinstance(n, int) or n < 0:
         raise ValueError('fibonacci is undefined for n = {!r}'.format(n))
     # Implement fibonacci_recursive, _memoized, and _dynamic below, then
     # change this to call your implementation to verify it passes all tests
@@ -27,17 +28,55 @@ def fibonacci_recursive(n):
 
 
 def fibonacci_memoized(n):
-    # TODO: Memoize the fibonacci function's recursive implementation here
-    pass
+    # This code was originally belief that fibonacci starts at '0' (e.g. fib(0) == 1, fib(2) == 2)
+    # This code was changed so fibonacci starts at 1 (e.g. fib(0) == 0, fib(2) == 1)
+    n = int(n) - 1
+    if (n < 0):
+        return 0
+    return fibonacci_memoized_helper(n, {})
     # Once implemented, change fibonacci (above) to call fibonacci_memoized
     # to verify that your memoized implementation passes all test cases
 
+def fibonacci_memoized_helper(n, htable):
+    if n < 0:
+        return 1
+    if n in htable:
+        return htable[n]
+    if n == 0 or n == 1:
+        htable[0] = 1
+        htable[1] = 1
+        return 1
+
+    htable[n] = fibonacci_memoized_helper(n - 1, htable) + fibonacci_memoized_helper(n - 2, htable)
+    return htable[n]
+
 
 def fibonacci_dynamic(n):
-    # TODO: Implement the fibonacci function with dynamic programming here
-    pass
+    # This code was originally belief that fibonacci starts at '0' (e.g. fib(0) == 1, fib(2) == 2)
+    # This code was changed so fibonacci starts at 1 (e.g. fib(0) == 0, fib(2) == 1)
+    n = int(n) - 1
+    if n < 0:
+        return 0
+    if (n == 0 or n == 1):
+        return 1
+
+    return fibonacci_dynamic_helper(n, 1, 1, 1)
     # Once implemented, change fibonacci (above) to call fibonacci_dynamic
     # to verify that your dynamic implementation passes all test cases
+
+def fibonacci_dynamic_helper(n, curr, total, prev):
+    # print("N " + str(n))
+    # print("Curr " + str(curr))
+    # print("Total " + str(total))
+    # print("Prev " + str(prev))
+    # print("_________")
+    if curr == n:
+        return total
+
+    return fibonacci_dynamic_helper(n, curr + 1, (total + prev), total)
+
+
+
 
 
 def main():
@@ -45,8 +84,11 @@ def main():
     args = sys.argv[1:]  # Ignore script file name
     if len(args) == 1:
         num = int(args[0])
+        start = time.time()
         result = fibonacci(num)
+        end = time.time()
         print('fibonacci({}) => {}'.format(num, result))
+        print("Time taken {}".format(end - start))
     else:
         print('Usage: {} number'.format(sys.argv[0]))
 
